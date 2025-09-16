7977e1465c5ccdb5bb851ba94711e8ae5b3ef198
Hyunju Lee <guswnee00@gmail.com>
2025-09-16 11:53:43 +0900

    update and delete memo

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -7,8 +7,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class MemoController {
 
-  private final Map<Long,  Memo> memoList = new HashMap<>();
+  private final Map<Long, Memo> memoList = new HashMap<>();
 
   @PostMapping("/memos")
-  public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto){
+  public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
     Memo memo = new Memo(requestDto);
 
     Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) : null;
@@ -34,8 +37,30 @@ public class MemoController {
   }
 
   @GetMapping("/memos")
-  public List<MemoResponseDto> getMemos(){
-    List<MemoResponseDto> memoResponseDtos = memoList.values().stream().map(MemoResponseDto::new).toList();
+  public List<MemoResponseDto> getMemos() {
+    List<MemoResponseDto> memoResponseDtos = memoList.values().stream()
+        .map(MemoResponseDto::new).toList();
     return memoResponseDtos;
+  }
+
+  @PutMapping("/memos/{id}")
+  public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+    if (memoList.containsKey(id)) {
+      Memo memo = memoList.get(id);
+      memo.update(requestDto);
+      return memo.getId();
+    } else {
+      throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+    }
+  }
+
+  @DeleteMapping("/memos/{id}")
+  public Long deleteMemo(@PathVariable Long id) {
+    if (memoList.containsKey(id)) {
+      memoList.remove(id);
+      return id;
+    } else {
+      throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+    }
   }
 }
